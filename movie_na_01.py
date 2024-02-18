5a1f5e67999987c742c0bd94498989a4753e99b8
motor115 <motor115@gmail.com>
2024-02-18 21:04:48 +0900

    Update movie_na_01.py

--- movie_na_01.py
+++ movie_na_01.py
@@ -7,7 +7,7 @@ import time
 
 # OpenAI APIキーの入力
 st.sidebar.title("Settings")
-api_key = st.sidebar.text_input("OpenAI API Key")
+api_key = st.sidebar.text_input("OpenAI API Keyを入力してください")
 openai.api_key = api_key
 
 
@@ -49,7 +49,7 @@ def get_text_from_video(file):
     params = {
     "model": "gpt-4-vision-preview",
     "messages": PROMPT_MESSAGES,
-    "max_tokens": 500,
+    "max_tokens": 1500,
 }
     
     response = openai.chat.completions.create(**params)
